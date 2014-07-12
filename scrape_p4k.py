cb21983d8c68ba6fe08bebcfb9231c5740bf3d07
Daniel Saber <dsaber@danbot.local>
2014-07-12 08:20:26 -0700

    Add Album Artwork scraping

--- scrape_p4k.py
+++ scrape_p4k.py
@@ -6,42 +6,58 @@ import bs4
 import pandas as pd
 
 
-def scrape_meta(num_pages=780):
+def scrape_meta(url_root='http://pitchfork.com/reviews/albums/', num_pages=780):
 
 	'''This function scrapes all-review related meta data 
 	on Pitchfork.com. This meta data includes the following fields:
 		- Artist
 		- Album
 		- Reviewer
-		- Score
 		- Album Art URL
 	'''
 
 
-	# to be converted to DataFrame
 	result = { 'Artist': 		[],
 			   'Album':  		[],
 			   'Reviewer': 		[],
 			   'Date':			[], 
-			   'Score': 		[],
 			   'Artwork':		[]
 			 }
 
 
-	# Pitchfork's review section
-	url_root = 'http://pitchfork.com/reviews/albums/'
-
 	for page in range(num_pages):
-		url = url_root + str(page)
+
+		if page == 0:
+			str_page = ""
+		else:
+			str_page = str(page) 
+
+		url = url_root + str_page
 		r = requests.get(url)
 		bs = bs4.BeautifulSoup(r.text) 
 
-		review_grid = bs.select('.object-grid')[0]
-		meta_data = review_grid.select('.info')
+		# primary container of Pitchfork reviews
+		obgrid = bs.select('.object-grid')[0]
 
-		for album in meta_data:
-			result['Artist'] = album.h1.text.encode('ascii', 'ignore')
-			result['Album']  = album.h2.text.encode('ascii', 'ignore')
-			result['Reviewer'] = album.h3.text.encode('ascii', 'ignore')[3:] # [3:] in order to get rid of "by"
-			result['']
+		# using said primary container to get the fields we need
+		rev_info = obgrid.select('.info')
+		rev_links = 
+		art_info = obgrid.select('.artwork')
+
+		for album, art in zip(rev_info, art_info):
+
+			# the Artist/Album/Reviewer/Date fields should be self-explanatory
+			result['Artist'].append(album.h1.text.encode('ascii', 'ignore'))
+			result['Album'].append(album.h2.text.encode('ascii', 'ignore'))
+			result['Reviewer'].append(album.h3.text.encode('ascii', 'ignore')[3:]) # [3:] in order to get rid of "by"
+			result['Date'].append(album.h4.text.encode('ascii', 'ignore'))
+
+			# the Album Art field is a bit trickier because of Pitchfork's peculiar HTML;
+			# I'm simply flagging it so you (as well as Future-Me) will know to be wary
+			temp_art = bs4.BeautifulSoup(art.select('.lazy')[0]['data-content'])
+			result['Artwork'].append(temp_art.img['src'])
+
+	return result
+
+
 		
