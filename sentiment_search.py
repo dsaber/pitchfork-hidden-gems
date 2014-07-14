e48a34d0c1c42bb872a7db8f74a5d89703eee252
Daniel Saber <dsaber@danbot.local>
2014-07-13 18:15:58 -0700

    Wrapping up, will begin sentiment search tomorrow

--- sentiment_search.py
+++ sentiment_search.py
@@ -5,3 +5,7 @@
 	# 	   must show up in far more 'positive' documents than 'negative' documents, 
 	#	   or vice versa. The "far more" is the threshold of interest.
 	# (C) Naive Bayes OR Logistic Regression (including tuning parameters)
+
+
+if __name__ == '__main__':
+	pass 
