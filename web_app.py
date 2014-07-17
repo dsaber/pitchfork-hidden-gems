1a1020f581d8ca0381a2c2f7c997f39262132032
Daniel Saber <dsaber@danbot.local>
2014-07-17 10:26:10 -0700

    Make prediction template and beginning of predict logic

--- web_app.py
+++ web_app.py
@@ -3,7 +3,7 @@ import sys
 sys.dont_write_bytecode = True
 
 # web things
-from flask import Flask
+from flask import Flask, render_template
 
 # data/database
 import psycopg2
@@ -19,6 +19,16 @@ app = Flask(__name__)
 @app.route('/')
 def home_page():
 	return '<h1>Hello!</h1>'
+
+# predicting and scoring logic
+@app.route('/predict/')
+def predict():
+	return render_template('predict.html')
+
+@app.route('/predict/show_score/<review_text>')
+def show_score():
+	pass 
+
 
 
 if __name__ == '__main__':
