090364ae72f6436c2455b7d882cee386a4d31f52
Daniel Saber <dsaber@danbot.local>
2014-07-17 09:09:21 -0700

    Complete predict

--- model/predict.py
+++ model/predict.py
@@ -1,8 +1,20 @@
 # for mapping logistic regression probability to score
 import map_scale as ms
 
-def predict_one(review_text, tfidf, logreg, scale, scoring_map):
+# for default scoring (i.e., when we have non-review sized input)
+from textblob import TextBlob
+import cPickle
+default_scale = cPickle.load(open('default_scale.pkl', 'r'))
 
-	transformed_review_text = tfidf.transform([review_text])
-	raw_log_prob = logreg.predict_proba(transformed_review_text)[:, 1][0]
-	return raw_log_prob, ms.map_to_scoring_map(raw_log_prob, scale, scoring_map)
+def predict_one(review_text, tfidf, logreg, scale, scoring_map, default_scale=default_scale):
+
+	# my model was trained on reviews, so it doesn't really work well with tweet-sized
+	# input, hence the ensembling
+	if len(review_text) > 1000: 
+		transformed_review_text = tfidf.transform([review_text])
+		raw_log_prob = logreg.predict_proba(transformed_review_text)[:, 1][0]
+		return raw_log_prob, ms.map_to_scoring_map(raw_log_prob, scale, scoring_map)
+	else:
+		# defaulting to TextBlob scoring
+		raw_tblob_score = TextBlob(review_text).sentiment.polarity
+		return raw_tblob_score, ms.map_to_scoring_map(raw_tblob_score, default_scale, scoring_map)
